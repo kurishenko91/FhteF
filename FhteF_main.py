4fe5007af2ae648744804210b8e772cf13093566
Kseniia <35366420+kurishenko91@users.noreply.github.com>
2024-03-22 13:23:59 +0100

    Update FhteF_main.py

--- FhteF_main.py
+++ FhteF_main.py
@@ -2,13 +2,14 @@ from FhteF_models import *
 from experiment import *
 from experiment_fair import *
 
-
 def main():
+    res=[]
+    res_f=[]
     for i,md in enumerate([2,3,4,5,10,50]):
         for j,t in enumerate([500,1000]):
             for dataset_flag in [1,2,3]:
-                res=experiment(dataset_flag,max_depth=md,T=t)
-                res_f=experiment_fair(dataset_flag,max_depth=md,T=t,alpha_list=[2**i for i in range(-2,-1)])
+                res.append(experiment(dataset_flag,max_depth=md,T=t))
+                res_f.append(experiment_fair(dataset_flag,max_depth=md,T=t,alpha_list=[2**i for i in range(-2,2)]))
     return res, res_f
     
 if __name__== "__main__":
